a5881ed185cdb82f322c943942e13e15e512f97f
LycaXros <33806408+LycaXros@users.noreply.github.com>
2019-12-04 17:47:17 -0400

    En clase Huyendo

--- PWEB01B/Controllers/ConsultaController.cs
+++ PWEB01B/Controllers/ConsultaController.cs
@@ -7,6 +7,7 @@ using System.Net;
 using System.Web;
 using System.Web.Mvc;
 using PWEB01B.Models;
+using PWEB01B.ViewModel;
 
 namespace PWEB01B.Controllers
 {
@@ -36,11 +37,38 @@ namespace PWEB01B.Controllers
                 data = data.Where(x => x.GeneroId == Gid.Value);
             }
 
+            var q = (from d in data
+                     group d by new { d.CineId, d.PeliculaId } into Carte
+                     select new
+                     {
+                         Carte.Key.CineId,
+                         Carte.Key.PeliculaId,
+                         Carte.FirstOrDefault().Cinema,
+                         Carte.FirstOrDefault().Pelicula,
+                         Carte.FirstOrDefault().Genero,
+                         Carte.FirstOrDefault().Direccion,
+                         Carte.FirstOrDefault().Telefono,
+                         Hora_Tandas = Carte.Select(x => x.Hora_Tanda)
+                     })
+                    .ToList()
+                    .Select(x => new Cartelera2ViewModel() {
+                        CineId = x.CineId,
+                        PeliculaId = x.PeliculaId,
+                        Cinema = x.Cinema,
+                        Telefono = x.Telefono,
+                        Pelicula = x.Pelicula,
+                        Direccion = x.Direccion,
+                        Genero = x.Genero,
+                        Hora_Tandas = x.Hora_Tandas
+                    });
+            ViewBag.Data = q.ToList();
+
             //ViewBag.PeliculaId = new SelectList(db.Peliculas, "PeliculaId", "Titulo", Pid);
             ViewBag.GeneroId = new SelectList(db.Generoes, "GeneroId", "Nombre", Gid);
             ViewBag.TandaId = new SelectList(db.Tandas, "Id", "Hora", Tid);
             ViewBag.CineId = new SelectList(db.Cines, "CineId", "Nombre", Cid);
-            return View(data.ToList());
+            //return View(data.ToList());
+            return View(q.ToList());
         }
 
         protected override void Dispose(bool disposing)
--- PWEB01B/ViewModel/CarteleraViewModel.cs
+++ PWEB01B/ViewModel/CarteleraViewModel.cs
@@ -9,12 +9,27 @@ namespace PWEB01B.ViewModel
     public class CarteleraViewModel
     {
         public System.Guid CineId { get; set; }
-        
+
 
         public Cine Cine { get; set; }
         public IEnumerable<Pelicula> Peliculas { get; set; }
         public IEnumerable<string> Tandas { get; set; }
 
         public string TandasString => string.Join(", ", Tandas.ToArray());
+    }
+    public class Cartelera2ViewModel
+    {
+        public System.Guid CineId { get; set; }
+        public int PeliculaId { get; set; }
+
+        public string Cinema { get; set; }
+        public string Pelicula { get; set; }
+        public string Genero { get; set; }
+        public string Direccion { get; set; }
+        public string Telefono { get; set; }
+        public IEnumerable<string> Hora_Tandas { get; set; }
+
+        public string Hora_Tanda => string.Join(", ", Hora_Tandas.ToArray());
+        
     }
 }
